11e0511b96d64c488f86964b24d87c35aefa2bad
Joakim Persson <joakimperssson@gmail.com>
2012-09-22 15:01:48 +0200

    CreateAlarm: Added the first iteration of the design for the CreateAlarm activity

--- src/edu/chalmers/dat255/group09/Alarmed/CreateAlarm.java
+++ src/edu/chalmers/dat255/group09/Alarmed/CreateAlarm.java
@@ -1,20 +1,33 @@
 package edu.chalmers.dat255.group09.Alarmed;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TimePicker;
 
 public class CreateAlarm extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_alarm);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_alarm);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_create_alarm, menu);
-        return true;
-    }
+		TimePicker timePicker = (TimePicker) findViewById(R.id.createAlarmTimePicker);
+		timePicker.setIs24HourView(true);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_create_alarm, menu);
+		return true;
+	}
+
+	public void onSetAlarmBtnPressed(View view) {
+		TimePicker timePicker = (TimePicker) findViewById(R.id.createAlarmTimePicker);
+		int hour = timePicker.getCurrentHour();
+		int minute = timePicker.getCurrentMinute();
+		Log.d("CreateAlarm", hour + ":" + minute);
+	}
 }
