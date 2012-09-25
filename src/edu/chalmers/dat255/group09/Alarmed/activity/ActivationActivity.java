bb4e6e6592adaf59ab48d0b1dee77d37644b91d8
Adrian Bjugård <adrian@bjugard.se>
2012-09-25 12:21:03 +0200

    AlarmActivation: added vibration

--- src/edu/chalmers/dat255/group09/Alarmed/activity/ActivationActivity.java
+++ src/edu/chalmers/dat255/group09/Alarmed/activity/ActivationActivity.java
@@ -1,38 +1,49 @@
 package edu.chalmers.dat255.group09.Alarmed.activity;
 
-import edu.chalmers.dat255.group09.Alarmed.R;
-import edu.chalmers.dat255.group09.Alarmed.R.layout;
-import edu.chalmers.dat255.group09.Alarmed.R.menu;
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import edu.chalmers.dat255.group09.Alarmed.R;
 
 public class ActivationActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activation);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_activation);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_activation, menu);
-        return true;
-    }
+		initVibration();
+	}
 
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+	private void initVibration() {
+		// get os vibration service
+		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+		// create vibration pattern
+		long[] pattern = { 0, 200, 500 };
+		
+		// start vibrating
+		v.vibrate(pattern, 0);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_activation, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 }
