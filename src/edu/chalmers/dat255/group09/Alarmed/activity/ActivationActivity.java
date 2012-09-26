5998e93905d190cfd8a13f40d3926b51c38ee0f8
Andreas Rolen <adderollen@hotmail.com>
2012-09-26 11:38:43 +0200

    alarmActivated: Added sound, button to stop the alarm and vibration

--- src/edu/chalmers/dat255/group09/Alarmed/activity/ActivationActivity.java
+++ src/edu/chalmers/dat255/group09/Alarmed/activity/ActivationActivity.java
@@ -1,33 +1,61 @@
 package edu.chalmers.dat255.group09.Alarmed.activity;
 
+import java.io.IOException;
+
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 import edu.chalmers.dat255.group09.Alarmed.R;
 
 public class ActivationActivity extends Activity {
 
+	private MediaPlayer mediaPlayer;
+	private Vibrator vibrator;
+
+	@SuppressLint({ "NewApi", "NewApi" })
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_activation);
 
-		initVibration();
+		Button stopAlarm = (Button) findViewById(R.id.stopAlarm);
+		stopAlarm.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				vibrator.cancel();
+				mediaPlayer.stop();
+				finish();
+				return false;
+			}
+		});
+		startVibrate(this);
+		playSound(this, getAlarmUri());
+
 	}
 
-	private void initVibration() {
-		// get os vibration service
-		Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+	private void startVibrate(Context context) {
+		vibrator = (Vibrator) context
+				.getSystemService(Context.VIBRATOR_SERVICE);
+		long[] vibPattern = { 0, 200, 500 };
 
-		// create vibration pattern
-		long[] pattern = { 0, 200, 500 };
-		
-		// start vibrating
-		v.vibrate(pattern, 0);
+		vibrator.vibrate(vibPattern, 0);
 	}
 
 	@Override
@@ -46,4 +74,33 @@ public class ActivationActivity extends Activity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	private void playSound(Context context, Uri alert) {
+		mediaPlayer = new MediaPlayer();
+		try {
+			mediaPlayer.setDataSource(context, alert);
+			final AudioManager audioManager = (AudioManager) context
+					.getSystemService(Context.AUDIO_SERVICE);
+			if (audioManager.getStreamVolume(AudioManager.STREAM_ALARM) != 0) {
+				mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+				mediaPlayer.prepare();
+				mediaPlayer.start();
+			}
+		} catch (IOException e) {
+			System.out.println("Sound Error");
+		}
+
+	}
+
+	private Uri getAlarmUri() {
+		Uri alert = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+		if (alert == null) {
+			alert = RingtoneManager
+					.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+			if (alert == null) {
+				alert = RingtoneManager
+						.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+			}
+		}
+		return alert;
+	}
 }
