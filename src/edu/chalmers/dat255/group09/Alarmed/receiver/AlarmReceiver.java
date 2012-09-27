1bfeacd734dda2e99515ae36912be4703dbbb5c7
Daniel Augurell <daniel.augurell@gmail.com>
2012-09-27 13:33:54 +0200

    Database: The alarms is now being removed from the database when activated

--- src/edu/chalmers/dat255/group09/Alarmed/receiver/AlarmReceiver.java
+++ src/edu/chalmers/dat255/group09/Alarmed/receiver/AlarmReceiver.java
@@ -1,5 +1,7 @@
 package edu.chalmers.dat255.group09.Alarmed.receiver;
 
+import edu.chalmers.dat255.group09.Alarmed.database.AlarmDbAdapter;
+import edu.chalmers.dat255.group09.Alarmed.model.Alarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -10,7 +12,14 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Toast.makeText(context, "Activated", Toast.LENGTH_SHORT).show();
-		Log.d("CreateAlarm: ", "Alarm Activated");
+		AlarmDbAdapter db = AlarmDbAdapter.getInstance();
+		db.openDb();
+		Alarm alarm = db.fetchAlarm(intent.getIntExtra("ID", -1));
+		if (alarm != null) {
+			Toast.makeText(context, "Activated", Toast.LENGTH_SHORT).show();
+			Log.d("CreateAlarm: ", "Alarm Activated");
+			db.deleteAlarm(alarm.getId());
+		}
 	}
+
 }
