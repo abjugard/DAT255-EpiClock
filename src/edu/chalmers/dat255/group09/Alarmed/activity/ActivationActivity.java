504195a123b2d3e5dc8da8134210cb4eb6e5e820
Adrian Bjugård <adrian@bjugard.se>
2012-09-27 10:09:47 +0200

    AlarmActivation: debug message to logcat instead of stdout

--- src/edu/chalmers/dat255/group09/Alarmed/activity/ActivationActivity.java
+++ src/edu/chalmers/dat255/group09/Alarmed/activity/ActivationActivity.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v4.app.NavUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
@@ -86,7 +87,7 @@ public class ActivationActivity extends Activity {
 				mediaPlayer.start();
 			}
 		} catch (IOException e) {
-			System.out.println("Sound Error");
+			Log.d("Sound", "Sound error");
 		}
 
 	}
