3541a3a4173248e7167847dfc68ac6adf8037823
Joakim Persson <joakimperssson@gmail.com>
2012-10-02 22:08:50 +0200

    taskImplementation: Implemented MathController
    Implemented MathController and added tests for its methods

---
+++ AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/modules/mathModule/model/MathControllerTest.java
@@ -0,0 +1,139 @@
+package edu.chalmers.dat255.group09.Alarmed.test.modules.mathModule.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.test.AndroidTestCase;
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.controller.MathController;
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.AdditionOperator;
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MathProblem;
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MathProblemGenerator;
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MultiplicationOperator;
+
+public class MathControllerTest extends AndroidTestCase {
+
+	private MathController controller;
+	private int[] correctAnswers = null;
+	private int problemIndex = 0;
+
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		MockMathProblemGenerator mockGenerator = new MockMathProblemGenerator();
+		controller = new MathController(mockGenerator);
+		correctAnswers = new int[] { 1, 4 };
+		problemIndex = 0;
+	}
+
+	public void testcheckAnswer() {
+
+		// a new problem is generated
+		controller.generateNewProblem();
+
+		int answer = 1;
+
+		assertTrue(controller.checkAnswer(answer));
+
+		// a new problem is generated
+		controller.generateNewProblem();
+
+		assertFalse(controller.checkAnswer(answer));
+
+		// the correct answer is 4
+		answer = 4;
+		assertTrue(controller.checkAnswer(answer));
+
+	}
+
+	public void testFiveCorrectAnswerInARow() {
+
+		int expectedCorrectAnswers = 0;
+		int actualCorrectAnswers = 0;
+		int answer = 0;
+
+		for (int i = 0; i < 5; i++) {
+			assertFalse(controller.isComplete());
+			answer = getCorrectAnswer();
+			controller.checkAnswer(answer);
+			expectedCorrectAnswers++;
+			actualCorrectAnswers = controller.getCompletedProblems();
+			assertEquals(expectedCorrectAnswers, actualCorrectAnswers);
+		}
+
+		assertTrue(controller.isComplete());
+	}
+
+	public void testGetCorrectAnswers() {
+
+		int expectedCorrectAnswers = 0;
+		int actualCorrectAnswers = 0;
+		int answer = 0;
+
+		/*
+		 * Two correct answers in a row
+		 */
+
+		for (int i = 0; i < 5; i++) {
+			answer = getCorrectAnswer();
+			controller.checkAnswer(answer);
+			expectedCorrectAnswers++;
+			actualCorrectAnswers = controller.getCompletedProblems();
+			assertEquals(expectedCorrectAnswers, actualCorrectAnswers);
+		}
+
+		/*
+		 * Simulate one wrong answer
+		 */
+		controller.generateNewProblem();
+		answer = -1;
+		controller.checkAnswer(answer);
+
+		expectedCorrectAnswers = 0;
+		actualCorrectAnswers = controller.getCompletedProblems();
+
+		assertEquals(expectedCorrectAnswers, actualCorrectAnswers);
+	}
+
+	private int getCorrectAnswer() {
+		controller.generateNewProblem();
+		int answer = correctAnswers[problemIndex];
+		problemIndex = (problemIndex + 1) % 2;
+		return answer;
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		controller = null;
+		correctAnswers = null;
+	}
+
+	/**
+	 * A mock implementation of the MathProblemGenerator class which now only
+	 * returns two different problems every other time. Which results in that
+	 * the first time controller.generateNewProblem() will return a problem with
+	 * answer 1 and the seconds time 4 and the third 1 and so on
+	 * 
+	 */
+	private class MockMathProblemGenerator extends MathProblemGenerator {
+
+		private int index = 0;
+		private List<MathProblem> problems;
+
+		public MockMathProblemGenerator() {
+			super();
+			problems = new ArrayList<MathProblem>();
+			problems.add(new MathProblem(new int[] { 1, 1 },
+					new MultiplicationOperator()));
+			problems.add(new MathProblem(new int[] { 2, 2 },
+					new AdditionOperator()));
+		}
+
+		@Override
+		public MathProblem generateProblem() {
+			MathProblem problem = problems.get(index);
+			index = (index + 1) % 2;
+			return problem;
+		}
+	}
+}
--- src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/controller/MathController.java
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/controller/MathController.java
@@ -1,26 +1,76 @@
 package edu.chalmers.dat255.group09.Alarmed.modules.mathModule.controller;
 
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MathOperator;
 import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MathProblem;
 import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MathProblemGenerator;
 
 public class MathController {
+
 	private MathProblem currentProblem;
 	private int completedProblemsInARow;
 	private MathProblemGenerator problemGenerator;
+	private final static int problemsThatMustBeCompleted = 5;
 
+	/**
+	 * Create a new MathController Object
+	 * 
+	 * @param generator
+	 *            The ProblemGenerator to be used
+	 */
+	public MathController(MathProblemGenerator generator) {
+		this.problemGenerator = generator;
+		this.completedProblemsInARow = 0;
+	}
+
+	/**
+	 * Checks if the user has answerd the current problem correctly
+	 * 
+	 * @param answer
+	 *            The Users Answer
+	 * @return If it was correct or not
+	 */
 	public boolean checkAnswer(int answer) {
-		return false;
+		MathOperator operator = currentProblem.getOperator();
+		int[] nbrs = currentProblem.getNumbers();
+
+		boolean correctAnswer = answer == operator.getResult(nbrs[0], nbrs[1]);
+
+		if (correctAnswer) {
+			completedProblemsInARow++;
+		} else {
+			completedProblemsInARow = 0;
+		}
+
+		return correctAnswer;
 	}
 
-	private boolean evaluateAnswer(int answer) {
-		return false;
-	}
-
-	public boolean isComplete() {
-		return false;
-	}
-
-	public MathProblem getProblem() {
+	/**
+	 * Get a new MathProblem
+	 * 
+	 * @return A new MathProblem
+	 */
+	public MathProblem generateNewProblem() {
+		currentProblem = problemGenerator.generateProblem();
 		return currentProblem;
 	}
+
+	/**
+	 * Returns true if the user has completed enough problems in a row and false
+	 * otherwise
+	 * 
+	 * @return If the user has answered correctly on enough problems
+	 */
+	public boolean isComplete() {
+		return completedProblemsInARow == problemsThatMustBeCompleted;
+	}
+
+	/**
+	 * Returns how many problems the user has solved in a row
+	 * 
+	 * @return Numbers of problems solved in a row
+	 */
+	public int getCompletedProblems() {
+		return completedProblemsInARow;
+	}
+
 }
