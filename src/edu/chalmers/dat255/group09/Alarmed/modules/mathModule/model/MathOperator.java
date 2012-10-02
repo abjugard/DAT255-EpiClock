0180eccd7ab9d67bfb694b5043f86e65bfc955bf
Joakim Persson <joakimperssson@gmail.com>
2012-10-02 18:31:36 +0200

    taskImplementation: Updated MathOperator
    Removed the toString method from the interface and added javadoc for
    the getResult method

--- src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/model/MathOperator.java
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/model/MathOperator.java
@@ -2,8 +2,15 @@ package edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model;
 
 public interface MathOperator {
 
-	public void getResult(int numberOne, int numberTwo);
-
-	public String toString();
+	/**
+	 * Evalutate the entered numbers using the decided operator
+	 * 
+	 * @param numberOne
+	 *            A number
+	 * @param numberTwo
+	 *            A number
+	 * @return The evaluate result of the two numbers
+	 */
+	public int getResult(int numberOne, int numberTwo);
 
 }
