1dda240166cc7fa92c71d69686963deddc6ab99a
Joakim Persson <joakimperssson@gmail.com>
2012-10-02 18:43:40 +0200

    taskImplementation: Added skeleton for MathController

--- src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/controller/MathController.java
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/controller/MathController.java
@@ -1,5 +1,26 @@
 package edu.chalmers.dat255.group09.Alarmed.modules.mathModule.controller;
 
-public class MathController {
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MathProblem;
+import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.MathProblemGenerator;
 
+public class MathController {
+	private MathProblem currentProblem;
+	private int completedProblemsInARow;
+	private MathProblemGenerator problemGenerator;
+
+	public boolean checkAnswer(int answer) {
+		return false;
+	}
+
+	private boolean evaluateAnswer(int answer) {
+		return false;
+	}
+
+	public boolean isComplete() {
+		return false;
+	}
+
+	public MathProblem getProblem() {
+		return currentProblem;
+	}
 }
