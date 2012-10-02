08a0b0aeb4520fbe157f784609d57fa91898c3fe
Joakim Persson <joakimperssson@gmail.com>
2012-10-02 16:17:25 +0200

    taskImplementation: Added an interface for math operators
    Added an interface representing math operators in this task and added a
    stub implementation for the Addition Operator

---
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/model/AdditionOperator.java
@@ -0,0 +1,15 @@
+package edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model;
+
+public class AdditionOperator implements MathOperator {
+
+	@Override
+	public void getResult(int numberOne, int numberTwo) {
+
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
+}
---
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/model/MathOperator.java
@@ -0,0 +1,9 @@
+package edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model;
+
+public interface MathOperator {
+
+	public void getResult(int numberOne, int numberTwo);
+
+	public String toString();
+
+}
