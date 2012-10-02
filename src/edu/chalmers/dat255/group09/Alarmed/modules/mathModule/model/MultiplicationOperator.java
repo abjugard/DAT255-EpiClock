ceddf8b4872e4230810803cf0ac9a97e65d47911
Joakim Persson <joakimperssson@gmail.com>
2012-10-02 18:42:01 +0200

    taskImplementation: Implemented MultiplicationOperator class
    Completed the implementation for MultiplicationOperator and added tests
    for the methods

---
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/mathModule/model/MultiplicationOperator.java
@@ -0,0 +1,16 @@
+package edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model;
+
+public final class MultiplicationOperator implements MathOperator {
+
+	private final static String OPERATOR = "*";
+
+	@Override
+	public int getResult(int numberOne, int numberTwo) {
+		return numberOne * numberTwo;
+	}
+
+	@Override
+	public String toString() {
+		return OPERATOR;
+	}
+}
