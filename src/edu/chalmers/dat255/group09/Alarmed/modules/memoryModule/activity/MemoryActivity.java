4c14b8e6269e19b50d1fbd639bfc4d4238d5fc3f
Joakim Persson <joakimperssson@gmail.com>
2012-10-12 00:55:42 +0200

    MemoryModule: Changed layout in MemoryActivity and added an adpter
    Changed from RelativeLayout to GridView and also added an adapter thats
    responsible for adding items to the view.

--- src/edu/chalmers/dat255/group09/Alarmed/modules/memoryModule/activity/MemoryActivity.java
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/memoryModule/activity/MemoryActivity.java
@@ -1,19 +1,63 @@
+/*
+ * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugard, Andreas Rolen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
 import edu.chalmers.dat255.group09.Alarmed.R;
+import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.adapter.MemoryAdapter;
+import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.model.Card;
+import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.views.CardImageButton;
 
-public class MemoryActivity extends Activity {
+/**
+ * 
+ * @author Joakim Persson
+ * 
+ */
+public class MemoryActivity extends Activity implements OnItemClickListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_memory);
+
+		List<CardImageButton> images = new ArrayList<CardImageButton>();
+
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		images.add(new CardImageButton(this, new Card(R.drawable.sample_image)));
+		GridView g = (GridView) findViewById(R.id.myGrid);
+		MemoryAdapter memoryAdapter = new MemoryAdapter(images);
+		g.setAdapter(memoryAdapter);
+		g.setNumColumns(3);
+		g.setOnItemClickListener(this);
 	}
 
 	@Override
@@ -23,9 +67,10 @@ public class MemoryActivity extends Activity {
 		return true;
 	}
 
-	public void onButtonPressed(View view) {
-		Log.d("CreateAlarm", "" + view.getId());
-		view.setBackgroundColor(Color.CYAN);
-	}
+	@Override
+	public void onItemClick(AdapterView<?> adapter, View view, int id, long numb) {
+		CardImageButton btn = (CardImageButton) view;
+		btn.toggleStatus();
 
+	}
 }
---
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/memoryModule/adapter/MemoryAdapter.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugard, Andreas Rolen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.adapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.views.CardImageButton;
+
+public class MemoryAdapter extends BaseAdapter {
+
+	private List<CardImageButton> images;
+
+	public MemoryAdapter(List<CardImageButton> cardImages) {
+		images = new ArrayList<CardImageButton>(cardImages);
+	}
+
+	@Override
+	public int getCount() {
+		return images.size();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return position;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		return position;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		CardImageButton view;
+		if (convertView == null) {
+			view = images.get(position);
+			view.setLayoutParams(new GridView.LayoutParams(160, 160));
+			view.setAdjustViewBounds(false);
+			view.setScaleType(ImageView.ScaleType.CENTER_CROP);
+			view.setPadding(8, 8, 8, 8);
+		} else {
+			view = (CardImageButton) convertView;
+		}
+
+		return view;
+	}
+}
