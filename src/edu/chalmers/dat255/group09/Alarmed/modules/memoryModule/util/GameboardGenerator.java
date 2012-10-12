72b595a34d9b1ac454dedc41432cde62ea6e77b7
Joakim Persson <joakimperssson@gmail.com>
2012-10-12 18:12:46 +0200

    MemoryModule: Moved GameboardGenerator into the utils package

--- src/edu/chalmers/dat255/group09/Alarmed/modules/memoryModule/util/GameboardGenerator.java
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/memoryModule/util/GameboardGenerator.java
@@ -1,4 +1,19 @@
-package edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.model;
+/*
+ * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugard, Andreas Rolen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.util;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,19 +22,19 @@ import java.util.Random;
 
 import android.content.Context;
 
-import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.util.ImageLoader;
+import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.model.Card;
 import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.views.CardImageButton;
 
 public class GameboardGenerator {
 
 	private Context context;
 	private ImageLoader imageLoader;
-	
+
 	public GameboardGenerator(Context context) {
 		this.context = context;
 		imageLoader = new ImageLoader();
 	}
-	
+
 	public List<CardImageButton> getGameBoard(int nbrOfpairs) {
 		List<CardImageButton> images = new ArrayList<CardImageButton>();
 
