5bc6e38e2e2aceab8ea2d5e8a3816b831446e3c4
Joakim Persson <joakimperssson@gmail.com>
2012-10-12 19:08:45 +0200

    MemoryModule: Implemented and added tests for MemoryController

--- src/edu/chalmers/dat255/group09/Alarmed/modules/memoryModule/controller/MemoryController.java
+++ src/edu/chalmers/dat255/group09/Alarmed/modules/memoryModule/controller/MemoryController.java
@@ -1,5 +1,48 @@
+/*
+ * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugard, Andreas Rolen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.controller;
 
-public class MemoryController {
+import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.views.CardImageButton;
 
+/**
+ * 
+ * @author Joakim Persson
+ * 
+ */
+public class MemoryController {
+	private int pairsLeft;
+
+	public MemoryController(int nbrOfPairs) {
+		this.pairsLeft = nbrOfPairs;
+	}
+
+	public boolean isGameOver() {
+		return pairsLeft == 0;
+	}
+
+	public boolean isCardsEqual(CardImageButton firstCard,
+			CardImageButton secondCard) {
+		return firstCard.equals(secondCard);
+	}
+
+	public void correctGuess() {
+		pairsLeft--;
+	}
+
+	public int getPairsLeft() {
+		return pairsLeft;
+	}
 }
