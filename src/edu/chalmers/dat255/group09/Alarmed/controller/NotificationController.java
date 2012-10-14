136fad2cde0cbb29d2edf93306061c5f794b8675
Daniel Augurell <daniel.augurell@gmail.com>
2012-10-14 20:32:42 +0200

    NotificationController: Added javadoc and licenseheader.

--- src/edu/chalmers/dat255/group09/Alarmed/controller/NotificationController.java
+++ src/edu/chalmers/dat255/group09/Alarmed/controller/NotificationController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugard, Andreas Rolen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package edu.chalmers.dat255.group09.Alarmed.controller;
 
 import android.app.Notification;
@@ -10,18 +25,30 @@ import edu.chalmers.dat255.group09.Alarmed.R;
 import edu.chalmers.dat255.group09.Alarmed.activity.MainActivity;
 import edu.chalmers.dat255.group09.Alarmed.model.Alarm;
 
+/**
+ * Controller class for the notifications.
+ * @author Andreas Rolen
+ *
+ */
 public class NotificationController {
 
 	private NotificationManager notificationManager;
 	private Context context;
 	private Alarm currentNotification;
 
-	public NotificationController(Context context) {
-		this.context = context;
-		this.notificationManager = (NotificationManager) context
+	/**
+	 * Constructor for the NotificationController. 
+	 * @param ctx The android context
+	 */
+	public NotificationController(Context ctx) {
+		this.context = ctx;
+		this.notificationManager = (NotificationManager) ctx
 				.getSystemService(Context.NOTIFICATION_SERVICE);
 	}
-
+	/**
+	 * Adds a new notification with the specified alarm.
+	 * @param alarm The alarm which should be set as the current notification.
+	 */
 	public void addNotification(Alarm alarm) {
 		deleteCurrentNotification();
 		if (alarm != null) {
@@ -31,6 +58,11 @@ public class NotificationController {
 		}
 	}
 
+	/**
+	 * Creates a new Notification with the data from the alarm.
+	 * @param alarm The alarm to be set as the current notification.
+	 * @return a Notification with the data from the alarm.
+	 */
 	private Notification buildNotification(Alarm alarm) {
 		Intent notificationIntent = new Intent(context, MainActivity.class);
 		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
@@ -49,7 +81,9 @@ public class NotificationController {
 
 		return notification;
 	}
-	
+	/**
+	 * Deletes the current Notification.
+	 */
 	private void deleteCurrentNotification() {
 		if (currentNotification != null) {
 			notificationManager.cancel(currentNotification.getId());
