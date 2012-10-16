b5dca6ddd594487cc2954a0ae609e987d0d294fa
Joakim Persson <joakimperssson@gmail.com>
2012-10-17 00:24:30 +0200

    AdditionProblemTest: Minor refactoring and added javadoc to util methods

--- AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/modules/mathModule/model/problemTypes/AdditionProblemTest.java
+++ AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/modules/mathModule/model/problemTypes/AdditionProblemTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugård, Andreas Rolén
+ * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugard, Andreas Rolen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,7 @@ import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.constants.Di
 import edu.chalmers.dat255.group09.Alarmed.modules.mathModule.model.problemTypes.AdditionProblem;
 
 /**
+ * An test class for the AdditionProblem class.
  * 
  * @author Joakim Persson
  * 
@@ -48,17 +49,21 @@ public class AdditionProblemTest extends AndroidTestCase {
 		assertEquals(expected, actual);
 	}
 
-	public void testGetResult() {
+	public void testGetResultForThreeNumbers() {
+		int actualResult = additionProblem.getResult(new int[] { 5, -5, 1 });
+		int expectedResult = 1;
+		assertEquals(expectedResult, actualResult);
+	}
+
+	public void testGetResultForSixPlusSixe() {
 		int actualResult = additionProblem.getResult(new int[] { 6, 6 });
 		int expectedResult = 12;
 		assertEquals(expectedResult, actualResult);
+	}
 
-		actualResult = additionProblem.getResult(new int[] { 2, -1 });
-		expectedResult = 1;
-		assertEquals(expectedResult, actualResult);
-
-		actualResult = additionProblem.getResult(new int[] { 5, -5, 1 });
-		expectedResult = 1;
+	public void testGetResultForTwoPlusMinusOne() {
+		int actualResult = additionProblem.getResult(new int[] { 2, -1 });
+		int expectedResult = 1;
 		assertEquals(expectedResult, actualResult);
 	}
 
@@ -67,8 +72,8 @@ public class AdditionProblemTest extends AndroidTestCase {
 		int upperLimit = 10;
 		int expectedNbrOfNumbers = 2;
 
-		testAdditionProblem(Difficulty.EASY, lowerLimit, upperLimit,
-				expectedNbrOfNumbers);
+		testAdditionProblemGenerateNumbers(Difficulty.EASY, lowerLimit,
+				upperLimit, expectedNbrOfNumbers);
 	}
 
 	public void testGenerateMediumProblems() {
@@ -76,8 +81,8 @@ public class AdditionProblemTest extends AndroidTestCase {
 		int upperLimit = 10;
 		int expectedNbrOfNumbers = 3;
 
-		testAdditionProblem(Difficulty.MEDIUM, lowerLimit, upperLimit,
-				expectedNbrOfNumbers);
+		testAdditionProblemGenerateNumbers(Difficulty.MEDIUM, lowerLimit,
+				upperLimit, expectedNbrOfNumbers);
 	}
 
 	public void testGenerateHardProblems() {
@@ -85,12 +90,28 @@ public class AdditionProblemTest extends AndroidTestCase {
 		int upperLimit = 500;
 		int expectedNbrOfNumbers = 3;
 
-		testAdditionProblem(Difficulty.HARD, lowerLimit, upperLimit,
-				expectedNbrOfNumbers);
+		testAdditionProblemGenerateNumbers(Difficulty.HARD, lowerLimit,
+				upperLimit, expectedNbrOfNumbers);
 	}
 
-	private void testAdditionProblem(Difficulty difficulty, int lowerLimit,
-			int upperLimit, int expectedNbrOfNumbers) {
+	/**
+	 * A method that runs several tests on the problems generateNumbers method.
+	 * For example it checks if the problem returns the expected number of
+	 * number, if they are in range or if the numbers are non negative. The
+	 * method carries out the test by running the same tests on different set of
+	 * random numbers.
+	 * 
+	 * @param difficulty
+	 *            The problems difficulty
+	 * @param lowerLimit
+	 *            The lower limit for the numbers in the problem.
+	 * @param upperLimit
+	 *            The upper limit for the numbers in the problem.
+	 * @param expectedNbrOfNumbers
+	 *            The expected numbers of numbers to be generated.
+	 */
+	private void testAdditionProblemGenerateNumbers(Difficulty difficulty,
+			int lowerLimit, int upperLimit, int expectedNbrOfNumbers) {
 		int[] nbrs = null;
 
 		for (int i = 0; i < ITERATIONS; i++) {
@@ -102,10 +123,31 @@ public class AdditionProblemTest extends AndroidTestCase {
 
 	}
 
+	/**
+	 * Checks that the number of numbers in an array is correct.
+	 * 
+	 * @param expectedNbrOfNumbers
+	 *            The expected number of numbers.
+	 * @param nbrs
+	 *            An array with number.
+	 * @return If array contains the expected number of numbers.
+	 */
 	private boolean isCorrectNbrOfNumbers(int expectedNbrOfNumbers, int[] nbrs) {
 		return expectedNbrOfNumbers == nbrs.length;
 	}
 
+	/**
+	 * Checks to see if the numbers in an array is within the specified limits.
+	 * [lowerLimit, upperLimit).
+	 * 
+	 * @param nbrs
+	 *            An array of numbers.
+	 * @param lowerLimit
+	 *            The lower limit for the numbers in the array.
+	 * @param upperLimit
+	 *            The upper limit for the numbers in the array.
+	 * @return If the numbers in the array is within the limit.
+	 */
 	private boolean isNumbersInRange(int[] nbrs, int lowerLimit, int upperLimit) {
 		for (int number : nbrs) {
 			if (number < lowerLimit || number >= upperLimit) {
@@ -116,6 +158,13 @@ public class AdditionProblemTest extends AndroidTestCase {
 		return true;
 	}
 
+	/**
+	 * Checks to see if numbers in the array is non negative.
+	 * 
+	 * @param numbers
+	 *            An array of numbers.
+	 * @return If the numbers in the array is non negative.
+	 */
 	private boolean isNumbersNonNegative(int[] numbers) {
 		for (int number : numbers) {
 			if (number < 0) {
