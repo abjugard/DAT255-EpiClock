387b39a8fbd29469c736851a43bb57e636921663
Joakim Persson <joakimperssson@gmail.com>
2012-10-16 17:25:05 +0200

    Refactor: Moved CarImageTest to the views package and added missing license

--- AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/modules/memoryModule/views/CardImageTest.java
+++ AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/modules/memoryModule/views/CardImageTest.java
@@ -1,4 +1,19 @@
-package edu.chalmers.dat255.group09.Alarmed.test.modules.memoryModule.model;
+/*
+ * Copyright (C) 2012 Joakim Persson, Daniel Augurell, Adrian Bjugard, Andreas Rolen
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.chalmers.dat255.group09.Alarmed.test.modules.memoryModule.views;
 
 import edu.chalmers.dat255.group09.Alarmed.R;
 import edu.chalmers.dat255.group09.Alarmed.modules.memoryModule.model.Card;
@@ -9,7 +24,7 @@ import android.test.AndroidTestCase;
 /**
  * 
  * @author Daniel Augurell
- *
+ * 
  */
 public class CardImageTest extends AndroidTestCase {
 	private Card card;
@@ -33,20 +48,24 @@ public class CardImageTest extends AndroidTestCase {
 		assertTrue(card.getStatus().equals(CardStatus.Visable));
 		assertEquals(IMAGE_RESOURCE, cardImage.getImageResource());
 	}
-	
+
 	public void testHashCodeAgainstSameImage() {
 		Card otherCard = new Card();
-		CardImage otherImage= new CardImage(getContext(), otherCard, IMAGE_RESOURCE);
+		CardImage otherImage = new CardImage(getContext(), otherCard,
+				IMAGE_RESOURCE);
 		assertEquals(cardImage.hashCode(), otherImage.hashCode());
 	}
+
 	public void testHashCodeAgainstOtherImage() {
 		Card otherCard = new Card();
 		CardImage otherImage = new CardImage(getContext(), otherCard, 0);
 		assertFalse(cardImage.hashCode() == otherImage.hashCode());
 	}
+
 	public void testHashCodeAgainstItself() {
 		assertEquals(cardImage.hashCode(), cardImage.hashCode());
 	}
+
 	public void testEqualsNull() {
 		assertFalse(cardImage.equals(null));
 	}
@@ -54,6 +73,7 @@ public class CardImageTest extends AndroidTestCase {
 	public void testEqualsItself() {
 		assertTrue(cardImage.equals(cardImage));
 	}
+
 	public void testEqualsAgainstOtherImage() {
 		Card otherCard = new Card();
 		CardImage otherImage = new CardImage(getContext(), otherCard, 0);
@@ -62,11 +82,13 @@ public class CardImageTest extends AndroidTestCase {
 
 	public void testEqualsSameImageOtherCardStatus() {
 		Card otherCard = new Card();
-		CardImage otherImage = new CardImage(getContext(), otherCard, IMAGE_RESOURCE);
+		CardImage otherImage = new CardImage(getContext(), otherCard,
+				IMAGE_RESOURCE);
 		otherImage.cardPressed();
 		assertFalse(cardImage.equals(otherImage));
 	}
-	public void testDisabled(){
+
+	public void testDisabled() {
 		cardImage.setDisabled();
 		assertTrue(cardImage.isDisabled());
 	}
