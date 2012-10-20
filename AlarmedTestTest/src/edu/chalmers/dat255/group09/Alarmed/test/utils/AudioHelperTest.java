514b11792b7f77061a2b7c553854619f6e963727
Adrian Bjugård <adrian@qng.se>
2012-10-20 16:29:21 +0200

    AlarmedTestTest: added tests for AlarmHelper

---
+++ AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/utils/AudioHelperTest.java
@@ -0,0 +1,25 @@
+package edu.chalmers.dat255.group09.Alarmed.test.utils;
+
+import android.content.Intent;
+import android.test.AndroidTestCase;
+import edu.chalmers.dat255.group09.Alarmed.utils.AudioHelper;
+
+public class AudioHelperTest extends AndroidTestCase {
+	private AudioHelper hAudio;
+	private Intent intent;
+
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		intent = new Intent();
+		hAudio = new AudioHelper(getContext(), intent);
+	}
+	
+	public void testGetAlarmToneDialog() {
+		assertNotNull(hAudio.getAlarmToneDialog());
+	}
+	
+	public void testGetVolumeDialog() {
+		assertNotNull(hAudio.getVolumeDialog());
+	}
+}
