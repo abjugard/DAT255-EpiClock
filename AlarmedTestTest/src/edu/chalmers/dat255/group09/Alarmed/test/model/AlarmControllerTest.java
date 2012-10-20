13b24c26b3a0c024153c1d26dc6d5866606cad28
Daniel Augurell <daniel.augurell@gmail.com>
2012-10-20 13:38:51 +0200

    AlarmControllerTest: Fixed AlarmControllerTest to use the current implementation

--- AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/model/AlarmControllerTest.java
+++ AlarmedTestTest/src/edu/chalmers/dat255/group09/Alarmed/test/model/AlarmControllerTest.java
@@ -42,8 +42,10 @@ public class AlarmControllerTest extends AndroidTestCase {
 		ac = AlarmController.getInstance();
 		handler = new MockAlarmHandler().openCon();
 		ac.init(context, handler);
-		ac.createAlarm(10, 10, 0, "", 0);
-		ac.createAlarm(20, 20, 0, "", 0);
+		Alarm a1 = new Alarm(10, 10, 0, "", 0);
+		Alarm a2 = new Alarm(20, 20, 0, "", 0);
+		ac.addAlarm(a1);
+		ac.addAlarm(a2);
 	}
 
 	public void testDestroy() {
@@ -112,10 +114,12 @@ public class AlarmControllerTest extends AndroidTestCase {
 			nbrID = 0;
 		}
 
-		public long createAlarm(int hour, int minute, int daysOfWeek, String module, int volume) {
-			Alarm alarm = new Alarm(hour, minute, ++nbrID, module, volume);
-			alarms.add(alarm);
-			return alarm.getId();
+		public long addAlarm(Alarm alarm) {
+			Alarm addAlarm = new Alarm(alarm.getAlarmHours(),
+					alarm.getAlarmMinutes(), ++nbrID, alarm.getModule(),
+					alarm.getVolume());
+			alarms.add(addAlarm);
+			return addAlarm.getId();
 		}
 
 		public boolean deleteAlarm(int alarmID) {
